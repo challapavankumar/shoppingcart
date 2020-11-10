25ac51c1f07a9e948a87ac18f75769b69f6a8b84
challapavankumar <65992313+challapavankumar@users.noreply.github.com>
2020-11-10 10:27:16 +0530

    first commit

--- shop/views.py
+++ shop/views.py
@@ -163,7 +163,6 @@ class Checkout(EcomerceMixin_view,CreateView):
         else:
             return redirect('/log_in/?next=/checkout/')
 
-            print('not logged in user')
         return super().dispatch(request, *args, **kwargs)
     def get_context_data(self, **kwargs):
         context=super().get_context_data(**kwargs)
